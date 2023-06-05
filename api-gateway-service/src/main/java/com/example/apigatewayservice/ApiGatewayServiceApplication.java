14a23990ab86817f9725d6f20294873b69fe2ae2
aphadatare <aniket.phadatare@mavenir.com>
2023-06-05 17:26:39 +0530

    Added discovery client annotation

--- api-gateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
